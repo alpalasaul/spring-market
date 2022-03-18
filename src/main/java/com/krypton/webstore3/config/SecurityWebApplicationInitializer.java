a504003c1c494785079fa9c9734088f66b649098
alpalasaul <alpalasaul@gmail.com>
2022-03-17 23:54:03 -0500

    Practica 30

---
+++ src/main/java/com/krypton/webstore3/config/SecurityWebApplicationInitializer.java
@@ -0,0 +1,7 @@
+package com.krypton.webstore3.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends
+        AbstractSecurityWebApplicationInitializer {
+}
